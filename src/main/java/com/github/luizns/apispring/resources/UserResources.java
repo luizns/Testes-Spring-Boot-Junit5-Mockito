91d02159f33393cd9d1ec0fe70160350dad19e55
Luiz Nascimento <luns.wm@hotmail.com>
2023-01-25 14:06:41 -0200

    criando interface user resource findById

--- src/main/java/com/github/luizns/apispring/resources/UserResources.java
+++ src/main/java/com/github/luizns/apispring/resources/UserResources.java
@@ -1,16 +1,23 @@
 package com.github.luizns.apispring.resources;
 
 import com.github.luizns.apispring.domain.User;
+import com.github.luizns.apispring.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/user")
 public class UserResources {
+
+    @Autowired
+    private UserService service;
+
     @GetMapping(value = "/{id}")
-    public ResponseEntity<User> findById() {
-return ResponseEntity.ok().body(new User(1L, "Luiz","luns.wm@gmail.com","123"));
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        return ResponseEntity.ok().body(service.findById(id));
     }
 }

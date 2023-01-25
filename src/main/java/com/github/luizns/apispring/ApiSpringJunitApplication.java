e57034455a0e61d15cc62d50472d557a7fcc7b99
Luiz Nascimento <luns.wm@hotmail.com>
2023-01-25 12:19:04 -0200

    projeto iniciado

---
+++ src/main/java/com/github/luizns/apispring/ApiSpringJunitApplication.java
@@ -0,0 +1,13 @@
+package com.github.luizns.apispring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSpringJunitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiSpringJunitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/luizns/apispring/ApiSpringJunitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.luizns.apispring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSpringJunitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

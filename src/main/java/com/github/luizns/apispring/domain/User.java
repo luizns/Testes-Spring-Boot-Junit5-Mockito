d00130aa6a6d667453cc5eeaafb401a61ab13246
Luiz Nascimento <luns.wm@hotmail.com>
2023-01-25 13:01:11 -0200

    criando a classe user

---
+++ src/main/java/com/github/luizns/apispring/domain/User.java
@@ -0,0 +1,21 @@
+package com.github.luizns.apispring.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Column(unique = true)
+    private String email;
+    private String password;
+}

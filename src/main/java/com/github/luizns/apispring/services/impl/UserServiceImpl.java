afb02171f24ada78840458d6674fa7ce3648d07a
Luiz Nascimento <luns.wm@hotmail.com>
2023-01-25 15:59:36 -0200

    tratamento de excecao para objeto não encontrado

--- src/main/java/com/github/luizns/apispring/services/impl/UserServiceImpl.java
+++ src/main/java/com/github/luizns/apispring/services/impl/UserServiceImpl.java
@@ -3,10 +3,9 @@ package com.github.luizns.apispring.services.impl;
 import com.github.luizns.apispring.domain.User;
 import com.github.luizns.apispring.repositories.UserRepository;
 import com.github.luizns.apispring.services.UserService;
+import com.github.luizns.apispring.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -16,8 +15,9 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User findById(Long id) {
-        Optional<User> obj = repository.findById(id);
-        return obj.orElse(null);
+        return this.repository
+                .findById(id)
+                .orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado"));
     }
 
 

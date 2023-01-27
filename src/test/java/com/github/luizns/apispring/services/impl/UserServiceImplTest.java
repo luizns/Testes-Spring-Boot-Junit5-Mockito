75e2bd13604483dd53913e15e7fc4f0f46ac9e47
Luiz Nascimento <luns.wm@hotmail.com>
2023-01-26 21:25:07 -0300

    teste para classe user service metodo findAll

--- src/test/java/com/github/luizns/apispring/services/impl/UserServiceImplTest.java
+++ src/test/java/com/github/luizns/apispring/services/impl/UserServiceImplTest.java
@@ -3,6 +3,7 @@ package com.github.luizns.apispring.services.impl;
 import com.github.luizns.apispring.domain.User;
 import com.github.luizns.apispring.domain.dto.UserDTO;
 import com.github.luizns.apispring.repositories.UserRepository;
+import com.github.luizns.apispring.services.exceptions.ObjectNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -11,6 +12,7 @@ import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
@@ -22,10 +24,12 @@ import static org.mockito.Mockito.when;
 @SpringBootTest
 class UserServiceImplTest {
 
-    public static final long ID         = 1L;
-    public static final String NAME     = "Luiz";
-    public static final String EMAIL    = "luiz@gmail.com";
+    public static final long ID = 1L;
+    public static final String NAME = "Luiz";
+    public static final String EMAIL = "luiz@gmail.com";
     public static final String PASSWORD = "123";
+    public static final String USUARIO_NAO_ENCONTRADO = "Usuário não encontrado";
+    public static final int INDEX = 0;
     @InjectMocks
     private UserServiceImpl service;
     @Mock
@@ -58,7 +62,32 @@ class UserServiceImplTest {
     }
 
     @Test
-    void findAll() {
+    void whenFindByIdThenReturnAnObjectNotFoundException() {
+        when(repository.findById(anyLong())).thenThrow(new ObjectNotFoundException(USUARIO_NAO_ENCONTRADO));
+
+        try {
+            service.findById(ID);
+        } catch (Exception ex) {
+            assertEquals(ObjectNotFoundException.class, ex.getClass());
+            assertEquals(USUARIO_NAO_ENCONTRADO, ex.getMessage());
+        }
+    }
+
+    @Test
+    void whenFindAllThenReturnAnListOfUsers() {
+        when(repository.findAll()).thenReturn(List.of(user));
+
+        List<User> response = service.findAll();
+
+        assertNotNull(response);
+        assertEquals(1, response.size());
+        assertEquals(User.class, response.get(INDEX).getClass());
+
+        assertEquals(ID, response.get(INDEX).getId());
+        assertEquals(NAME, response.get(INDEX).getName());
+        assertEquals(EMAIL, response.get(INDEX).getEmail());
+        assertEquals(PASSWORD, response.get(INDEX).getPassword());
+
     }
 
     @Test
